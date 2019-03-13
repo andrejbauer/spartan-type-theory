985530a5840cacd569e1ea45bcbc21392223a595
Anja Petković <anja.petkovic@fmf.uni-lj.si>
2019-03-13 18:28:10 +0100

    fixed a bug in checking equality of application

--- src/equal.ml
+++ src/equal.ml
@@ -13,32 +13,32 @@ let rec norm_expr ~strategy ctx e =
   | TT.Type -> e
 
   | TT.Atom x ->
-     begin
-       match Context.lookup_def x ctx with
-       | None -> e
-       | Some e -> norm_expr ~strategy ctx e
-     end
+    begin
+      match Context.lookup_def x ctx with
+      | None -> e
+      | Some e -> norm_expr ~strategy ctx e
+    end
 
   | TT.Prod _ -> e
 
   | TT.Lambda _ -> e
 
   | TT.Apply (e1, e2) ->
-     let e1 = norm_expr ~strategy ctx e1
-     and e2 =
-       begin
-         match strategy with
-         | WHNF -> e2
-         | CBV -> norm_expr ~strategy ctx e2
-       end
-     in
-     begin
-       match e1 with
-       | TT.Lambda (_, e') ->
-          let e' = TT.instantiate 0 e2 e' in
-          norm_expr ~strategy ctx e'
-       | _ -> TT.Apply (e1, e2)
-     end
+    let e1 = norm_expr ~strategy ctx e1
+    and e2 =
+      begin
+        match strategy with
+        | WHNF -> e2
+        | CBV -> norm_expr ~strategy ctx e2
+      end
+    in
+    begin
+      match e1 with
+      | TT.Lambda (_, e') ->
+        let e' = TT.instantiate 0 e2 e' in
+        norm_expr ~strategy ctx e'
+      | _ -> TT.Apply (e1, e2)
+    end
 
 (** Normalize a type *)
 let norm_ty ~strategy ctx (TT.Ty ty) =
@@ -62,26 +62,26 @@ let rec expr ctx e1 e2 ty =
     match  ty' with
 
     | TT.Prod ((x, t), u) ->
-       (* Apply function extensionality. *)
-       let x' = TT.new_atom x in
-       let ctx = Context.extend_ident x' t ctx
-       and e1 = TT.Apply (e1, TT.Atom x')
-       and e2 = TT.Apply (e2, TT.Atom x')
-       and u = TT.unabstract_ty x' u in
-       expr ctx e1 e2 u
+      (* Apply function extensionality. *)
+      let x' = TT.new_atom x in
+      let ctx = Context.extend_ident x' t ctx
+      and e1 = TT.Apply (e1, TT.Atom x')
+      and e2 = TT.Apply (e2, TT.Atom x')
+      and u = TT.unabstract_ty x' u in
+      expr ctx e1 e2 u
 
     | TT.Type
     | TT.Apply _
     | TT.Bound _
     | TT.Atom _ ->
-       (* Type-directed phase is done, we compare normal forms. *)
-       let e1 = norm_expr ~strategy:WHNF ctx e1
-       and e2 = norm_expr ~strategy:WHNF ctx e2 in
-       expr_whnf ctx e1 e2
+      (* Type-directed phase is done, we compare normal forms. *)
+      let e1 = norm_expr ~strategy:WHNF ctx e1
+      and e2 = norm_expr ~strategy:WHNF ctx e2 in
+      expr_whnf ctx e1 e2
 
     | TT.Lambda _ ->
-       (* A type should never normalize to an abstraction *)
-       assert false
+      (* A type should never normalize to an abstraction *)
+      assert false
   end
 
 (** Structurally compare weak head-normal expressions [e1] and [e2]. *)
@@ -91,45 +91,45 @@ and expr_whnf ctx e1 e2 =
   | TT.Type, TT.Type -> true
 
   | TT.Bound k1, TT.Bound k2 ->
-     (* We should never be in a situation where we compare bound variables,
-        as that would mean that we forgot to unabstract a lambda or a product. *)
-     assert false
+    (* We should never be in a situation where we compare bound variables,
+       as that would mean that we forgot to unabstract a lambda or a product. *)
+    assert false
 
   | TT.Atom x, TT.Atom y -> x = y
 
   | TT.Prod ((x, t1), u1), TT.Prod ((_, t2), u2)  ->
-     ty ctx t1 t2 &&
-     begin
-       let x' = TT.new_atom x in
-       let ctx = Context.extend_ident x' t1 ctx
-       and u1 = TT.unabstract_ty x' u1
-       and u2 = TT.unabstract_ty x' u2 in
-       ty ctx u1 u2
-     end
+    ty ctx t1 t2 &&
+    begin
+      let x' = TT.new_atom x in
+      let ctx = Context.extend_ident x' t1 ctx
+      and u1 = TT.unabstract_ty x' u1
+      and u2 = TT.unabstract_ty x' u2 in
+      ty ctx u1 u2
+    end
 
   | TT.Lambda ((x, t1), e1), TT.Lambda ((_, t2), e2)  ->
-     (* We should never have to compare two lambdas, as that would mean that the
-        type-directed phase did not figure out that these have product types. *)
-     assert false
+    (* We should never have to compare two lambdas, as that would mean that the
+       type-directed phase did not figure out that these have product types. *)
+    assert false
 
   | TT.Apply (e11, e12), TT.Apply (e21, e22) ->
-     let rec collect sp1 sp2 e1 e2 =
-       match e1, e2 with
-       | TT.Apply (e11, e12), TT.Apply (e21, e22) ->
-          collect (e12 :: sp1) (e22 :: sp2) e11 e21
-       | TT.Atom a1, TT.Atom a2 ->
-          Some ((a1, sp1), (a2, sp2))
-       | _, _ -> None
-     in
-     begin
-       match collect [e12] [e22] e11 e21 with
-       | None -> false
-       | Some ((a1, sp1), (a2, sp2)) -> spine ctx (a1, sp1) (a2, sp2)
-     end
+    let rec collect sp1 sp2 e1 e2 =
+      match e1, e2 with
+      | TT.Apply (e11, e12), TT.Apply (e21, e22) ->
+        collect (e12 :: sp1) (e22 :: sp2) e11 e21
+      | TT.Atom a1, TT.Atom a2 ->
+        Some ((a1, sp1), (a2, sp2))
+      | _, _ -> None
+    in
+    begin
+      match collect [e12] [e22] e11 e21 with
+      | None -> false
+      | Some ((a1, sp1), (a2, sp2)) -> spine ctx (a1, sp1) (a2, sp2)
+    end
 
 
   | (TT.Type | TT.Bound _ | TT.Atom _ | TT.Prod _ | TT.Lambda _ | TT.Apply _), _ ->
-     false
+    false
 
 (** Compare two types. *)
 and ty ctx (TT.Ty ty1) (TT.Ty ty2) =
@@ -139,27 +139,27 @@ and ty ctx (TT.Ty ty1) (TT.Ty ty2) =
 
     A spine is a nested application of the form [a e1 e2 ... en]
     where [a] is an atom.
- *)
+*)
 and spine ctx (a1, sp1) (a2, sp2) =
   a1 = a2 &&
   begin
     let rec fold ty sp1 sp2 =
-      match sp1, sp2 with
-      | [e1], [e2] -> expr ctx e1 e2 ty
-      | e1 :: sp1, e2 :: sp2 ->
-         begin
-           match as_prod ctx ty with
-           | None -> assert false
-           | Some ((x, t), u) ->
-              expr ctx e1 e2 t &&
-                begin
-                  let u = TT.instantiate_ty 0 e1 u in
-                  fold u sp1 sp2
-                end
-         end
-      | _, _ ->
-         (* We should never be here, as the lengths of the spines should match. *)
-         assert false
+      match as_prod ctx ty with
+      | None -> assert false
+      | Some ((x, t), u) -> 
+        begin
+          match sp1, sp2 with
+          | [e1], [e2] -> expr ctx e1 e2 t 
+          | e1 :: sp1, e2 :: sp2 ->
+            expr ctx e1 e2 t &&
+            begin
+              let u = TT.instantiate_ty 0 e1 u in
+              fold u sp1 sp2
+            end
+          | _, _ ->
+            (* We should never be here, as the lengths of the spines should match. *)
+            assert false
+        end 
     in
     match Context.lookup_atom_ty a1 ctx with
     | None -> assert false
